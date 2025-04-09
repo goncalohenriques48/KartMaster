a56112d236703745d368169ae334d3a860414541
Gonçalo <150794244+goncalohenriques48@users.noreply.github.com>
2025-04-09 13:07:16 +0100

    migração novo modelo dados

--- Migrations/20250409120615_AlteracaoModelo.Designer.cs
+++ Migrations/20250409120615_AlteracaoModelo.Designer.cs
@@ -4,6 +4,7 @@ using KartMaster.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KartMaster.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250409120615_AlteracaoModelo")]
+    partial class AlteracaoModelo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
