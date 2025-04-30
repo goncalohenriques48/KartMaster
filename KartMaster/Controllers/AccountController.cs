743a0d55f2fc8bb3d06d3056a87bacde9b19f535
Rodrigo Henriques <rodrigomch7@gmail.com>
2025-04-30 11:12:31 +0100

    Criação do scafold do login e registo e adição de uma "drawer" ao clicar no icon do user na página inicial, para registo e login do utilizador.

---
+++ KartMaster/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KartMaster.Models;
+
+namespace KartMaster.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(Utilizador model)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(Utilizador model)
+        {
+
+            return RedirectToAction("Login");
+        }
+    }
+}
